15ed69925f423cef8aae0f889083c24b7e4f864e
oscarkhima <boukhimaoscar@gmail.com>
2024-10-28 20:23:51 +0100

    grid out

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,27 +4,18 @@ import './styles/fonts.scss';
 
 function App() {
 
-  const numDivs = 10;
-
   return (
     <>
-    <section className='bacgroudGrid'>
-
-    {[...Array(numDivs)].map((_, index) => (
-        <div key={index} className="mi-div">
-           <div className="line"></div>
-        </div>
-      ))}
-     
-
-    </section>
+   
       <section className="maintenance">
         <div className="contentMaintenance">
           <div className="title syne ">WE'RE WORKING ON SOMETHING NEW</div>
           <div className="subtitle syne">CHECK BACK SOON</div>
         </div>
 
-        <div className="caption syne">Oscar Khima - A web Designer & Developper</div>
+        <div className="caption roboto-flex ">
+          <p className='capitionTitle'>Oscar Khima - A web Designer & Developper</p>
+        </div>
       </section>
     </>
   );
